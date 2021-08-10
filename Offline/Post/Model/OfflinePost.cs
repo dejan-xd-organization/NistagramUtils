ce0310c4525da2bfc4909f1a4d6572629a56f383
Dejan Jovanović <dejan.jovanovi94@gmail.com>
2021-08-10 22:46:45 +0200

    Close #3

---
+++ Offline/Post/Model/OfflinePost.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NistagramUtils.Offline.Post.Model
+{
+    public class OfflinePost
+    {
+        private long id;
+        private User user;
+        private DateTime timePublished;
+        private string postImage;
+        private int likes;
+        private int dislikes;
+        private string postDescription;
+        private List<OfflinePostComments> offlinePostComments;
+
+
+    }
+}
---
+++ Offline/Post/Services/PostServicesImpl.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NistagramUtils.Offline.Post.Model;
+
+namespace NistagramUtils.Offline.Post.Services
+{
+    public class PostServicesImpl : IPostServices
+    {
+        public HashSet<OfflinePost> GetAllOfflinePosts()
+        {
+            return new HashSet<OfflinePost>(0);
+        }
+
+        public HashSet<User> GetAllNewUsers()
+        {
+            return new HashSet<User>(0);
+        }
+    }
+}

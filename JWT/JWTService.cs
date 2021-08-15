f51f7f7509ce3b489b7db8c437879e053234610b
Dejan Jovanović <dejan.jovanovi94@gmail.com>
2021-08-16 00:12:31 +0200

    Close #1

---
+++ JWT/JWTService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NistagramSQLConnection.Model;
+
+namespace NistagramUtils.JWT
+{
+    public static class JWTService
+    {
+        private static Dictionary<string, User> activeUsers = new Dictionary<string, User>();
+
+        public static bool AddActiveUser(string jwt, User user)
+        {
+            User u = null;
+            if (!activeUsers.TryGetValue(jwt, out u))
+            {
+                if(!activeUsers.TryAdd(jwt, user))
+                {
+                    return true;
+                }
+                return false;
+            }
+            else
+            {
+                if (!activeUsers.Remove(jwt))
+                {
+                    return false;
+                }
+                return activeUsers.TryAdd(jwt, user);
+            }
+        }
+    }
+}

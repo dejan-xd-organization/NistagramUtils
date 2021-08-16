53a9442040823da239e6dae98c4cfdb4b9082fd8
Dejan Jovanović <dejan.jovanovi94@gmail.com>
2021-08-16 21:50:45 +0200

    fixing NistagramUtils project

---
+++ DTO/Login/LoginResponseDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NistagramUtils.DTO
+{
+    public class LoginResponseDTO
+    {
+        public string status;
+        public string jwt;
+        public UserDTO userDTO;
+    }
+}

2a22768ec79f7d01ac003f531d2364f479e7b4ca
Dejan Jovanović <dejan.jovanovi94@gmail.com>
2021-08-27 20:43:36 +0200

    Close #23

--- DTO/WallPost/WallPostDTO.cs
+++ DTO/WallPost/WallPostDTO.cs
@@ -1,7 +1,7 @@
-﻿using NistagramSQLConnection.Model;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using NistagramSQLConnection.Model;
 
 namespace NistagramUtils.DTO.WallPost
 {

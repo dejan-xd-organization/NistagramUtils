3e55e5c14f447da8d400c0bc8e8428623e7275a5
Dejan Jovanović <dejan.jovanovi94@gmail.com>
2021-08-31 16:08:15 +0200

    initial work

---
+++ DTO/Chat/SendMessageDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NistagramUtils.DTO.Chat
+{
+    public class SendMessageDto
+    {
+        public string text { get; set; }
+        public long userId { get; set; }
+        public long friendId { get; set; }
+    }
+}
